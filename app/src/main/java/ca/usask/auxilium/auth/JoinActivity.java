030fde0f412f374bb13e56f2a9b47ccf6c062372
Marvi Baloch (mab598) <mab598@mail.usask.ca>
2018-02-01 12:11:51 -0600

    feat(join): setting up Join feat

---
+++ app/src/main/java/ca/usask/auxilium/auth/JoinActivity.java
@@ -0,0 +1,16 @@
+
+package ca.usask.auxilium.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.usask.auxilium.R;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}

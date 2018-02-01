7b6b77b60e56abcd27ffd5a284531cc4c8511e52
Marvi Baloch (mab598) <mab598@mail.usask.ca>
2018-02-01 11:41:59 -0600

    feat(welcomeJoinCreate): create welcome activity

---
+++ app/src/main/java/ca/usask/auxilium/auth/WelcomeJoinCreateActivity.java
@@ -0,0 +1,14 @@
+package ca.usask.auxilium.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ca.usask.auxilium.R;
+
+public class WelcomeJoinCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_join_create);
+    }
+}

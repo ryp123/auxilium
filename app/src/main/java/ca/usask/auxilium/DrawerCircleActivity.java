943684945b46bddf3d50e984c6b9d27e7b278f05
Jason <goc558@mail.usask.ca>
2018-02-04 13:09:49 -0600

    feat(*): add drawer layout of circles feature
    add activity_drawer_circle layout
    add drawer_circle_menu xml
    add DrawerCircleActivity

---
+++ app/src/main/java/ca/usask/auxilium/DrawerCircleActivity.java
@@ -0,0 +1,17 @@
+package ca.usask.auxilium;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gongcheng on 2018-02-04.
+ */
+
+public class DrawerCircleActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_circle);
+    }
+}

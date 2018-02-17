fb0578c03617ce6b93124a9e89d660917d6a3d58
Josh Kocur (jak472) <josh.kocur@usask.ca>
2018-02-17 16:34:59 -0600

    feat(groupchat): add groupchat functionality
    -post message to screen after sending
    -store message in database after sending

---
+++ app/src/main/java/ca/usask/auxilium/Chat.java
@@ -0,0 +1,163 @@
+package ca.usask.auxilium;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Chat extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button btn_send_msg;
+    private EditText input_msg;
+    private TextView chat_conversation;
+    private FirebaseAuth mAuth;
+    private String circleID;
+
+    private String user_name,room_name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot() ;
+    private String temp_key;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+
+        //Some of these will be used later.
+        btn_send_msg = (Button) findViewById(R.id.btn_send);
+        input_msg = (EditText) findViewById(R.id.msg_input);
+        chat_conversation = (TextView) findViewById(R.id.textView);
+        mAuth = FirebaseAuth.getInstance();
+        circleID = mAuth.getCurrentUser().getUid();
+        Bundle bundle = getIntent().getExtras();
+        String newString = bundle.getString("room_name");
+
+
+        btn_send_msg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String,Object> map = new HashMap<String, Object>();
+                temp_key = root.push().getKey();
+                root.updateChildren(map);
+
+                DatabaseReference message_root = root;
+                Map<String,Object> map2 = new HashMap<String, Object>();
+                map2.put("name",mAuth.getCurrentUser().getDisplayName().toString());
+                map2.put("msg",input_msg.getText().toString());
+
+                message_root.updateChildren(map2);
+            }
+        });
+
+
+        root.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                append_chat_conversation(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                append_chat_conversation(dataSnapshot);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+       //getMenuInflater().inflate(R.menu.chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        return true;
+    }
+
+    private String chat_msg,chat_user_name;
+
+    private void append_chat_conversation(DataSnapshot dataSnapshot) {
+
+        Iterator i = dataSnapshot.getChildren().iterator();
+
+        while (i.hasNext()){
+
+            chat_msg = (String) ((DataSnapshot)i.next()).getValue();
+            chat_user_name = (String) ((DataSnapshot)i.next()).getValue();
+
+            chat_conversation.append(chat_user_name +" : "+chat_msg +" \n");
+        }
+
+
+    }
+}
---
+++ app/src/main/java/ca/usask/auxilium/ChatLanding.java
@@ -0,0 +1,82 @@
+package ca.usask.auxilium;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ChatLanding extends AppCompatActivity {
+
+    private Button send;
+    private EditText message;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_messages = new ArrayList<>();
+    private String name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_landing);
+
+        send = (Button) findViewById(R.id.btn_add_room);
+        message = (EditText) findViewById(R.id.room_name_edittext);
+        listView = (ListView) findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+        listView.setAdapter(arrayAdapter);
+
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put("Message: ","");
+                String newMessage = message.getText().toString();
+
+                //Post message to screen
+                arrayAdapter.add(newMessage);
+
+                //Add to database -- change later to make it adaptive.  Find a way to auto increment
+                root.child("Circles").child("Circle 2").child("Chat").child("Concerned Group").updateChildren(map);
+                root.child("Circles").child("Circle 2").child("Chat").child("Concerned Group").child(newMessage).setValue(newMessage);
+                //Clear
+                message.setText("");
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+

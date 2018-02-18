19303676ef2df7a4d0efefb119ea5a0d41463da3
jjball <jadenjball@gmail.com>
2018-02-17 20:09:49 -0600

    feat: saving/loading data from db on the profile and edit profile screens

--- app/src/main/java/ca/usask/auxilium/auth/IndexProfileActivity.java
+++ app/src/main/java/ca/usask/auxilium/auth/IndexProfileActivity.java
@@ -8,43 +8,87 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import ca.usask.auxilium.Circle;
 import ca.usask.auxilium.R;
+import ca.usask.auxilium.User;
 
 public class IndexProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private GoogleSignInAccount mAcct;
+    Circle circle;
+
+    ImageView imgProfilePic;
+    TextView txtFirstName;
+    TextView txtLastName;
+    TextView txtPrefName;
+    TextView txtAge;
+    TextView txtGender;
+    TextView txtDiagnosis;
+    TextView txtEmergencyContact;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index_profile_page);
+
+        mAcct = GoogleSignIn.getLastSignedInAccount(this);
         mAuth = FirebaseAuth.getInstance();
+        String userName = mAcct.getDisplayName();
+        circle = new Circle();
+        circle.setCircleName("JadenTestCircle");
 
-        ImageView imgProfilePic = findViewById(R.id.profilePicture);
-        TextView txtFirstName = findViewById(R.id.indexFirstName);
-        TextView txtLastName = findViewById(R.id.indexLastName);
-        TextView txtPrefName = findViewById(R.id.indexPrefName);
-        TextView txtAge = findViewById(R.id.indexAge);
-        TextView txtGender = findViewById(R.id.indexGender);
-        TextView txtDiagnosis = findViewById(R.id.indexDiagnosis);
-        TextView txtEmergencyContact = findViewById(R.id.emergencyContact);
+        imgProfilePic = findViewById(R.id.profilePicture);
+        txtFirstName = findViewById(R.id.indexFirstName);
+        txtLastName = findViewById(R.id.indexLastName);
+        txtPrefName = findViewById(R.id.indexPrefName);
+        txtAge = findViewById(R.id.indexAge);
+        txtGender = findViewById(R.id.indexGender);
+        txtDiagnosis = findViewById(R.id.indexDiagnosis);
+        txtEmergencyContact = findViewById(R.id.emergencyContact);
 
-        if(mAuth.getCurrentUser() != null) {
-            //user logged in
-            Log.d("AUTH", mAuth.getCurrentUser().getEmail());
-
-            String[] fullName = mAuth.getCurrentUser().getDisplayName().split(" ");
-//            Uri profileURI = mAuth.getCurrentUser().getPhotoUrl();
-//            if(profileURI != null) {
-//                imgProfilePic.setImageURI(null);
-//                imgProfilePic.setImageURI(profileURI);
-//            }
-            txtFirstName.setText(fullName[0]);
-            txtLastName.setText(fullName[fullName.length - 1]);
-            txtEmergencyContact.setText(mAuth.getCurrentUser().getPhoneNumber());
+        if(userName == null){
+            Log.e("Google Account Error", "Display name is null");
+            return;
         }
+        Log.e("PROFILE USERNAME", userName);
+        FirebaseDatabase.getInstance().getReference()
+                .child("Circles")
+                .child(circle.getCircleName())
+                .child("Concerned")
+                .child(userName)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        if(user != null) {
+                            txtFirstName.setText(user.getFirstName());
+                            txtLastName.setText(user.getLastName());
+                            txtPrefName.setText(user.getPreferredName());
+                            txtAge.setText(user.getAge());
+                            txtGender.setText(user.getGender());
+                            txtDiagnosis.setText(user.getDiagnosis());
+                            txtEmergencyContact.setText(user.getEmergencyContact());
+                        }
+                        else{
+                            Log.e("FirebaseError", "User is null");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        throw databaseError.toException();
+                    }
+                });
 
 
     }
---
+++ app/src/main/java/ca/usask/auxilium/auth/ProfileEditActivity.java
@@ -0,0 +1,130 @@
+package ca.usask.auxilium.auth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ca.usask.auxilium.Circle;
+import ca.usask.auxilium.R;
+import ca.usask.auxilium.User;
+
+/**
+ * Created by jadenball on 2018-02-17.
+ */
+
+public class ProfileEditActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInAccount mAcct;
+    private DatabaseReference mDatabase;
+    Circle circle;
+
+    ImageView imgProfilePic;
+    EditText txtFirstName;
+    EditText txtLastName;
+    EditText txtPrefName;
+    EditText txtAge;
+    EditText txtGender;
+    EditText txtDiagnosis;
+    EditText txtEmergencyContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAcct = GoogleSignIn.getLastSignedInAccount(this);
+        mAuth = FirebaseAuth.getInstance();
+        String userName = mAcct.getDisplayName();
+        final String uid = mAuth.getCurrentUser().getUid();
+
+        circle = new Circle();
+        circle.setCircleName("JadenTestCircle");
+
+        imgProfilePic = findViewById(R.id.profilePicture);
+        txtFirstName = findViewById(R.id.indexFirstName);
+        txtLastName = findViewById(R.id.indexLastName);
+        txtPrefName = findViewById(R.id.indexPrefName);
+        txtAge = findViewById(R.id.indexAge);
+        txtGender = findViewById(R.id.indexGender);
+        txtDiagnosis = findViewById(R.id.indexDiagnosis);
+        txtEmergencyContact = findViewById(R.id.emergencyContact);
+
+        if(userName == null){
+            Log.e("Google Account Error", "Display name is null");
+            return;
+        }
+        Log.e("PROFILE USERNAME", userName);
+        FirebaseDatabase.getInstance().getReference()
+                .child("Circles")
+                .child(circle.getCircleName())
+                .child("Concerned")
+                .child(userName)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                if(user != null) {
+                    txtFirstName.setText(user.getFirstName());
+                    txtLastName.setText(user.getLastName());
+                    txtPrefName.setText(user.getPreferredName());
+                    txtAge.setText(user.getAge());
+                    txtGender.setText(user.getGender());
+                    txtDiagnosis.setText(user.getDiagnosis());
+                    txtEmergencyContact.setText(user.getEmergencyContact());
+                }
+                else{
+                    Log.e("FirebaseError", "User is null");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_edit_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                User user = new User(txtFirstName.getText().toString(),
+                        txtLastName.getText().toString(),
+                        txtPrefName.getText().toString(),
+                        txtAge.getText().toString(),
+                        txtGender.getText().toString(),
+                        txtDiagnosis.getText().toString(),
+                        txtEmergencyContact.getText().toString());
+                user.setUserName(mAcct.getDisplayName());
+                user.setStatus("Active");
+                mDatabase.child("Circles").child(circle.getCircleName()).child("Concerned").child(user.getUserName()).setValue(user);
+                Log.d("FirebaseSave", "Edited user data saved");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
+}

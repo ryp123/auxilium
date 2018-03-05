df39281552b28953f57b548830eeff8a36d2d5ad
szelic <s.cheng@usask.ca>
2018-03-04 21:03:11 -0600

    test(InvitationUnitTest): add unit tests for Invitation Class' setters and getters

---
+++ app/src/test/java/ca/usask/auxilium/InvitationsUnitTest.java
@@ -0,0 +1,51 @@
+package ca.usask.auxilium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sherry on 2018-03-04.
+ */
+
+/**  tests all setters and getters in Invitations Class for functionality
+ *   does not test for valid/non-valid inputs
+ */
+public class InvitationsUnitTest {
+
+    String testCircleID = "1";
+    String testEmail = "test@test.com";
+    Long testLong = 123L;
+
+    Invitations i = new Invitations(testCircleID, testEmail);
+
+    @Test
+    public void setCircleId_and_getCircleId_test() throws Exception {
+        assertSame("1", i.getCircleId());
+        i.setCircleId("2");
+        assertNotNull(i.getCircleId());
+        assertSame("2", i.getCircleId());
+    }
+
+    @Test
+    public void setEmail_and_getEmail_test() throws Exception {
+        assertSame("test@test.com", i.getEmail());
+        i.setEmail("testEmail");
+        assertNotNull(i.getCircleId());
+        assertSame("testEmail", i.getEmail());
+    }
+
+    @Test
+    public void setSentDynamicLink_and_getSentDynamicLink_test() throws Exception {
+        assertFalse(i.getSentDynamicLink());
+        i.setSentDynamicLink(true);
+        assertTrue(i.getSentDynamicLink());
+    }
+
+    @Test
+    public void setExpirationDate_and_getExpirationDate_test() throws Exception {
+        i.setExpirationDate(testLong);
+        assertEquals(testLong, i.getExpirationDate());
+    }
+
+}

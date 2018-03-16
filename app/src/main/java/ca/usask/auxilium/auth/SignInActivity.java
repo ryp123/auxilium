a095c9943517d37401edfd2fceeed64e3a1f237d
jjball <jadenjball@gmail.com>
2018-03-16 12:56:29 -0600

    style: removal of unneeded commented out code

--- app/src/main/java/ca/usask/auxilium/auth/SignInActivity.java
+++ app/src/main/java/ca/usask/auxilium/auth/SignInActivity.java
@@ -186,12 +186,8 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
         if (user != null) {
-            //mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
             mStatusTextView.setText(null);
-
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
         } else {
             mStatusTextView.setText(R.string.signed_out);
             mDetailTextView.setText(null);

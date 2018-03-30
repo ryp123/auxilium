36f65b04178a637dd897edfa09032f51877e91b4
Josh Kocur (jak472) <josh.kocur@usask.ca>
2018-03-30 15:47:44 -0600

    fix conflict

--- app/src/main/java/ca/usask/auxilium/IndexStatusActivity.java
+++ app/src/main/java/ca/usask/auxilium/IndexStatusActivity.java
@@ -45,6 +45,7 @@ public class IndexStatusActivity extends Fragment {
     private TextView lastSeenBy;
     private TextView lastType;
     private TextView assesment;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
     private View myView;
     private FirebaseUser fUser;
     private String userId;

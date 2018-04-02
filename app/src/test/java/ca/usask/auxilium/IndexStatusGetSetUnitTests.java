9b6be74c227210acef4e93d93a1cd200aa04735e
Marvi Baloch (mab598) <mab598@mail.usask.ca>
2018-04-02 14:26:30 -0600

    refactor(IndexStatusGetSetUnitTests): remove unused imports

--- app/src/test/java/ca/usask/auxilium/IndexStatusGetSetUnitTests.java
+++ app/src/test/java/ca/usask/auxilium/IndexStatusGetSetUnitTests.java
@@ -3,9 +3,6 @@ package ca.usask.auxilium;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
